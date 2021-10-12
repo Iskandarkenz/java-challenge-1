ca589079bfccd63b28b9eaad6c3ba585d8716465
Iskandarkenz <miskandarrasyid11@gmail.com>
2021-10-12 13:04:50 +0700

    first commit

---
+++ BasicJava3.java
@@ -0,0 +1,29 @@
+public class BasicJava3 {
+    public static void main(String args[]) {
+        // Chalenge Buat Biodata Diri
+        String Garis1         = "______________________________________";
+        String Fullname      = " M IskandarKenz      -";
+        String Gender        = " Laki-Laki           -";
+        String Status        = " Mahasiswa           -";
+        String Age           = " 17                  -";
+        String Address       = " Jl Manchester V     -";
+        String PhoneNumber   = " 0123456789          -";
+        String Hobby         = " Main                -";
+        String FavoriteFood  = " yang enak           -";
+        String FavoriteColor = " Gradient            -";
+        String Garis2        = "______________________________________";
+        // pembatas hehehe
+        System.out.println (Garis1); 
+        System.out.println ("Fullname      : "+ Fullname);
+        System.out.println ("Gender        : "+ Gender);
+        System.out.println ("Status        : "+ Status);
+        System.out.println ("Age           : "+ Age);
+        System.out.println ("Address       : "+ Address);
+        System.out.println ("PhoneNumber   : "+ PhoneNumber);
+        System.out.println ("Hobby         : "+ Hobby);
+        System.out.println ("FavoriteFood  : "+ FavoriteFood);
+        System.out.println ("FavoriteColor : "+ FavoriteColor);  
+        System.out.println (Garis2); 
+     }
+    }
+
---
+++ JAVA-BASIC-1/BasicJava.java
@@ -0,0 +1,16 @@
+package JAVA-BASIC-1;
+public class BasicJava 
+public static void main(String args[]) {
+String firstname = " M ";
+String midname = " Iskandar ";
+String lastname = " Kenz ";
+int integerage = 17;
+String address = "jatim";
+int phonenumber = 0123 ;
+String hobi = "main";
+String makanankesukaan = "yg enak";
+
+System.out.println(firstname + midname + lastname + integerage + address + phonenumber + hobi + makanankesukaan );
+
+}
+}
